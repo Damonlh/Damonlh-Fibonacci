d10ceb4b71197f94ab2e47c5da831b464104e86a
Damonlh <myorderrecipt@gmail.com>
2023-02-15 17:15:20 +0000

    Commit

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,13 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+
+        if (n < 2)
+        {
+            return n;
+            
+        }
+
+        return fib(n - 1) + fib(n - 2) ;
     }
 }
